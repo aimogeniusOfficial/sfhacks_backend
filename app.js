8e0626b9431735014faa6538b8cb0cc2065df8ae
aimogeniusOfficial <aimogenius@berkeley.edu>
2025-04-05 13:23:02 -0700

    Mongo DB

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var geminiRouter = require('./routes/gemini');
-
+var mongoRouter = require('./routes/mongo');
 
 var app = express();
 
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/rate', geminiRouter); //eto dlya gemini
-
+app.use('/mongo', mongoRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/mongo.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+var express = require('express');
+const { MongoClient, ObjectId } = require('mongodb');
+
+var router = express.Router();
+const uri = process.env.MONGO_URI;
+const client = new MongoClient(uri);
+let accessibilityCatalogueCollection;
+
+(async () => {
+    await client.connect();
+    accessibilityCatalogueCollection = client.db(process.env.MONGO_DB_NAME).collection('accessibility_catalogue');
+    console.log('Connected to MongoDB');
+})();
+
+router.post('/create', async (req, res) => {
+    const result = await accessibilityCatalogueCollection.insertOne(req.body);
+    res.status(201).json(result);
+});
+
+router.get('/', async (req, res) => {
+    const result = await accessibilityCatalogueCollection.find({}).toArray();
+    res.status(200).json(result);
+});
+
+module.exports = router;
