56f2d5e2a35e6b22d6b3761f01103e3d834681d2
Nurzhan Abdrassilov <nurzhan11abdrassilov@gmail.com>
2025-04-04 22:17:37 -0700

    nurzhan

--- play_around.js
+++ play_around.js
@@ -1,5 +1,24 @@
-function add(a, b) {
-    return a + b;
+import fetch from 'node-fetch';
+import { JSDOM } from 'jsdom';
+
+async function getCSS(url) {
+    const res = await fetch(url);
+    const html = await res.text()
+
+    const dom = new JSDOM(html);
+    const document = dom.window.document;
+    
+    const inlineStyles = Array.from(document.querySelectorAll('style')).map(style => style.textContent);
+
+    const cssLinks = Array.from(document.querySelectorAll('link[rel="stylesheet"]')).map(link => new URL(link.getAttribute('href'), url).href);
+
+    const externalCSS = await Promise.all(
+        cssLinks.map(async (cssUrl) => {
+            const res = await fetch(cssUrl);
+            return res.text();
+        })
+    );
+    return{inline: inlineStyles, external: externalCSS}
 }
 
-console.log(add(1, 2));
+getCSS('https://www.apple.com/').then(css => console.log(css)).catch(err => console.error('Error fetching CSS:', err))
