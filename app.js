9d23de11f76e583c3a378d069ee77f5f7c552be8
aimogeniusOfficial <aimogenius@berkeley.edu>
2025-04-05 13:39:08 -0700

    remove users router and resolve conflicts

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var geminiRouter = require('./routes/gemini');
 var mongoRouter = require('./routes/mongo');
 
@@ -22,7 +21,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/rate', geminiRouter); //eto dlya gemini
 app.use('/mongo', mongoRouter);
 
